998008df0c81af5ec9a8430d9575722d46e82ef8
Shotaro Nakamura <shotaro.nakamura.j8f@denso-wave.com>
2022-09-30 17:22:34 +0900

    first commit

---
+++ Maui1/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Maui1;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Maui1/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace Maui1;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnClicked(object sender, EventArgs e)
+	{
+		labelCount.Text = $"{count}";
+		count++;
+		labelTime.Text = DateTime.Now.ToString("HH:mm:ss");
+	}
+}
+
